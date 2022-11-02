fc9ac68be3ca82391e08cf3c90e666e5d5d6fcc2
Ana Cornachi <85295984+anacornachi@users.noreply.github.com>
2022-11-02 19:48:56 -0300

    first mobile commit

---
+++ src/screens/SignIn.tsx
@@ -0,0 +1,11 @@
+import {Center, Text} from 'native-base';
+
+export default function SignIn() {
+  return (
+    <Center flex={1} bg="gray.900">
+      <Text color="white" fontSize={24} fontFamily="heading">
+        Hello React Native
+      </Text>
+    </Center>
+  );
+}
